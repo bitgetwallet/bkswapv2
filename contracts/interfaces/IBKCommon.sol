1d041f99935936eb4f7b5bac79640316ed241314
eden <eden.du@bitget.com>
2023-11-08 18:23:37 +0800

    feat: commit BridgeAggregationFeature

--- contracts/interfaces/IBKCommon.sol
+++ contracts/interfaces/IBKCommon.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.17;
 
 interface IBKCommon {
-    function setOperator(address[] memory _operators, bool _isOperator) external; 
+    function setOperator(address[] calldata _operators, bool _isOperator) external; 
     
     function pause() external;
 
--- contracts/interfaces/IBKRegistry.sol
+++ contracts/interfaces/IBKRegistry.sol
@@ -6,11 +6,11 @@ interface IBKRegistry {
 
     function getFeature(bytes4 _methodId) external view returns(address proxy, bool isLib);
 
-    function setCallTarget(bytes4 _methodId, address [] memory _targets, bool _isEnable) external;
+    function setCallTarget(bytes4 _methodId, address[] calldata _targets, bool _isEnable) external;
 
     function isCallTarget(bytes4 _methodId, address _target) external view returns(bool);
 
-    function setApproveTarget(bytes4 _methodId, address [] memory _targets, bool _isEnable) external;
+    function setApproveTarget(bytes4 _methodId, address[] calldata _targets, bool _isEnable) external;
 
     function isApproveTarget(bytes4 _methodId, address _target) external view returns(bool);
 }
